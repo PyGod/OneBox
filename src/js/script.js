09adced252de6cec079de1bc4c1bae1cce016fc4
Alexanrd Romanenkov <pythonloveyou@gmail.com>
2018-07-03 16:44:20 +0300

    Changed custom select

--- src/js/script.js
+++ src/js/script.js
@@ -19,12 +19,34 @@ $(()=>{
 		
 	});
 
-	$('.select-field ul li').on('click', function() {
-		var label = $(this).parent().parent().children('label');
-		label.attr('data-value', $(this).attr('data-value'));
-		label.html($(this).html());
-		$('.header-left a').css('outline', 'none');
-		$('.header-left a').css('color', '#000');
-	})
+//Custom Select
+$('.select-field').on('mousedown', function() {
+	var label = $(this).parent().parent().children('label');
+	label.attr('data-value', $(this).attr('data-value'));
+	label.html($(this).html());
+	$('.select-field').addClass('active');
+})
+
+$('.select-field').on('mouseover', function() {
+	var label = $(this).parent().parent().children('label');
+	label.attr('data-value', $(this).attr('data-value'));
+	label.html($(this).html());
+	$('.select-field').css({'box-shadow' : 'inset 0px 3px 3px 1px #dfe5ed', 'cursor' : 'pointer'});
+})
+
+$('.select-field').on('mouseout', function() {
+	var label = $(this).parent().parent().children('label');
+	label.attr('data-value', $(this).attr('data-value'));
+	label.html($(this).html());
+	$('.select-field').css('box-shadow' , '0px 3px 3px 1px #dfe5ed');
+})
+
+$('.select-field ul li').on('mouseup', function() {
+	var label = $(this).parent().parent().children('label');
+	label.attr('data-value', $(this).attr('data-value'));
+	label.html($(this).html());
+	$('.header-left a').css('color', '#000');
+	$('.select-field').toggleClass('active', false);
+})
 
 })
